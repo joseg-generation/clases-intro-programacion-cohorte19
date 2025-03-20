3146f3ddb31213662f7d7625427ac539f1206e3f
Jose G Martinez <160265975+joseg-generation@users.noreply.github.com>
2025-03-20 13:11:58 +0000

    Add hello world script and initial variable file

---
+++ 01-helloworld.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cúal es tu nombre?: ")
+
+apellido = input("¿Cúal es tu apellido?: ")
+
+
+print("Tu nombre completo es: " + nombre + " " + apellido)
+print(f"Tu nombre completo es: {nombre} {apellido} y tu edad es {30 + 7}")
