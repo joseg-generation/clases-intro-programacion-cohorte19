62606fffc67bca954d5ad99d965f1a5e50cd2fdd
Jose G Martinez <160265975+joseg-generation@users.noreply.github.com>
2025-03-20 15:28:47 +0000

    Update comments in variables.js to clarify variable declaration rules

--- variables.js
+++ variables.js
@@ -5,8 +5,8 @@ var edad = 40
 
 //disponibles en el leguaje desde ecma6 2015
 
-// crear variables con let
+// crear variables con let (se puede redeclarar)
 let nombre = 'Juan';
 
-// crear variables con const
+// crear variables con const (no se puede redeclarar)
 const datos_empleado = 'Perez';
