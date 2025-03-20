43d633d0cf62a6591589d1a09972489b194b2948
Jose G Martinez <160265975+joseg-generation@users.noreply.github.com>
2025-03-20 15:28:17 +0000

    Add comments to variable declarations in variables.js

--- variables.js
+++ variables.js
@@ -1,10 +1,12 @@
 
-edad = 40
+// crear variables con var
+var edad = 40
 
+
+//disponibles en el leguaje desde ecma6 2015
+
+// crear variables con let
 let nombre = 'Juan';
 
+// crear variables con const
 const datos_empleado = 'Perez';
-
-let sueldo = 1000;
-
-let esDeDia = true;
