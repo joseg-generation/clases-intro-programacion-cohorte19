7e84e9df5565adb2e1dcce691b2fcb8f75580cbb
Jose G Martinez <160265975+joseg-generation@users.noreply.github.com>
2025-03-20 15:24:54 +0000

    Add variable definitions and examples in Python and JavaScript

---
+++ 02-variables.py
@@ -0,0 +1,45 @@
+
+"""
+Este es un comentario de 
+varias líneas.
+Muchas gracias
+"""
+
+# definimos una variable
+
+# Enteros (integer) int
+edad = 56
+asistencia = 40
+
+# Flotantes (float)
+peso_1 = 85.9
+peso_2= 80.6
+
+resultado = peso_1 - peso_2
+
+# Cadena de texto (string) tipo texto
+
+nombre = "Juan"
+texto_de_ejemplo = "Esto es un texto de ejemplo que incluye caracteres especiales como: #, $, %, & y números 1234567890"
+
+tipo_de_dato = "123456789"  # tipo de dato ????? string o cadena de texto
+
+# Booleanos (boolean) True o False
+
+puerta_abierta = False
+print(f"Estado inicial: {puerta_abierta}")
+
+## Lógica que cambia el estado de la puerta
+
+puerta_abierta = True
+print(f"Estado final: {puerta_abierta}")
+
+# utilizamos la variable
+# print(resultado)
+
+
+nombre_estudiante = "Alberto"
+
+apellido_estudiante = "Pérez"
+
+
---
+++ variables.js
@@ -0,0 +1,10 @@
+
+edad = 40
+
+let nombre = 'Juan';
+
+const datos_empleado = 'Perez';
+
+let sueldo = 1000;
+
+let esDeDia = true;
