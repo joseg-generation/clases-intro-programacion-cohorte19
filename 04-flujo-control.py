452ebb20f2942871d18a6c7deaf4e211b43b6e11
Jose G Martinez <160265975+joseg-generation@users.noreply.github.com>
2025-03-25 16:14:19 +0000

    Add conditional examples and comments in JavaScript and Python

---
+++ 04-flujo-control.py
@@ -0,0 +1,49 @@
+# condicionales 
+# Sentencia if
+
+name = "Alberto"
+
+# if "info" == "INFO":   # True o False
+#     print("El resultado es True")
+# else:
+#     print("El resultado es False")
+
+# if (4*6) == 25:
+#     print("Son iguales")
+
+# if 3 % 3 == 0:
+#     print("El resto es 0")
+
+# if 6 % 3 == 0:  ## es divisible
+#     print("El resto es cero")
+
+# food = input("Cual es tu comida favorita?: ")
+
+
+# if food == "arroz" or food == "arros":
+#     print("No es tan divertida esa comida.")
+# elif food == "hamburguesa":
+#     print("Esa comida si me gusta")
+# elif food == "pasta":
+#     print("Me encanta la comida italiana.")
+# elif food == "pasta con carne":
+#     print("Oh que bueno, también me gusta es combinación")
+# else:
+#     print("Nunca he escuchado sobre ese plato.")
+
+# 
+
+# suma +
+# resta -
+# multiplicación *
+# división /
+# módulo % (el resto de una división)
+# mayor que > 
+# menor que <
+# igualdad ==
+
+
+if 1==1 and 2==3 or 20==20 and 10==5 or 1==1:
+    print("Es true")
+else:
+    print("No se cumplen las dos condiciones en true")
--- variables.js
+++ variables.js
@@ -13,4 +13,18 @@ nombre = "Jose"
 // crear variables con const (no se puede redeclarar)
 const datos_empleado = 'Perez';
 
-datos_empleado = 'Lopez' // no se puede reasignar
+//datos_empleado = 'Lopez' // no se puede reasignar
+
+// igualdad
+if (1 == true) {
+
+    console.log("Comparar el valor sin tomar en cuenta el tipo de dato")
+    
+}
+
+//Estricta igualdad
+if ("1" === 1) {
+
+    console.log("Es true")
+    
+}
