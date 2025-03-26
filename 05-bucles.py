6a5499040d0c04c5360af8f77d9cc7c3973ac108
Jose G Martinez <160265975+joseg-generation@users.noreply.github.com>
2025-03-26 14:32:11 +0000

    Add while and for loop examples in 05-bucles.py

---
+++ 05-bucles.py
@@ -0,0 +1,18 @@
+
+
+# bucle while
+
+limit = 12
+
+counter = 1
+while counter < limit:
+    print(counter)
+    counter = counter + 1
+
+
+# bucle for
+
+names_i_like = ['Thomson', 'Thompson', 'Haddock', 'Snowy']
+
+for name in names_i_like:
+    print(f"I like this name: {name}")
