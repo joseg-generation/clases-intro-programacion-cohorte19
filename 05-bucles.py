0005a562e99bc4d4d72ee31272fb35f8cedba922
Jose G Martinez <160265975+joseg-generation@users.noreply.github.com>
2025-03-26 14:40:46 +0000

    Add additional examples of using for loops with user input in 05-bucles.py

--- 05-bucles.py
+++ 05-bucles.py
@@ -16,3 +16,12 @@ names_i_like = ['Thomson', 'Thompson', 'Haddock', 'Snowy']
 
 for name in names_i_like:
     print(f"I like this name: {name}")
+
+
+
+# otro ejemplo con range
+num = int(input("Ingresa el número máximo para jugar: "))
+
+
+for num in range(1,num +1):
+    print(num)
